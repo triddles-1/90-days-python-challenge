f21af223b0170209ac42b3a938e953414d91e27b
triddles Lamar <lamartriddles@gmail.com>
2025-01-07 00:01:19 +0000

    second day of my 90 days python challenge

---
+++ day02.py
@@ -0,0 +1,14 @@
+#Day 2 of the 90 days python challenge
+#an app to takes user name, age and calculate your date of birth
+
+#input user name
+name=input("Please Enter your name:  ")
+#input user age
+age=input("What is your age?   ")
+#calculate year of birth
+YOB=2025-int(age)
+#print out the informations
+print(f"Hello, {name}")
+print(f"you are {age} years old")
+print(f"your year of birth is {YOB}")
+
