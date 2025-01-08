01e93d1f6807e53a677f6a715f45ddc78c68fb61
triddles Lamar <lamartriddles@gmail.com>
2025-01-08 02:10:50 +0000

    day 03 of my 90 day python challenge

---
+++ day03.py
@@ -0,0 +1,8 @@
+#build a project to determine if you're allowed or not using your date
+#input age
+age=int(input("what is your age?  "))
+if age >= 18:
+    print("you are allowed")
+else:
+    print("you are not allowed")
+
