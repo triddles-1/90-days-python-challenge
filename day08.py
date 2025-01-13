cc5897f3c1e1a84f3096e4e83815c497a3f03363
triddles Lamar <lamartriddles@gmail.com>
2025-01-13 14:53:30 +0000

    day 08 of my challenge

--- day08.py
+++ day08.py
@@ -1,1 +1,14 @@
 # a program to read a file and count how many lines
+#read an absolute file
+with open(r"C:\Users\Triddles lamar\Downloads\test.txt") as file:
+    print("These are the content of the txt file")
+    print("====================================================================")
+    #assign lines variable to read file lines
+    lines=file.readlines()
+    #loop through file for every line
+    for line in lines:
+        print(line, end=' ')
+    line_numbers=len(lines)
+    #print number of lines
+    print(f"\nThe number oof lines in the txt file is {line_numbers} lines")
+    
